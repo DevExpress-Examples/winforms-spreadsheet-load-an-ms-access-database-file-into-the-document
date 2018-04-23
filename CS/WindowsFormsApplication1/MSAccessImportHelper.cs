15e05544c146eda474d262fea23eaa94097eeb0f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication1/MSAccessImportHelper.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.Office.Import;
+using DevExpress.Office.Internal;
+using DevExpress.Spreadsheet;
+using DevExpress.XtraSpreadsheet;
+using DevExpress.XtraSpreadsheet.Commands;
+using DevExpress.XtraSpreadsheet.Import;
+using DevExpress.XtraSpreadsheet.Internal;
+using DevExpress.XtraSpreadsheet.Services;
+
+namespace AccessImport {
+    // custom DocumentImportManagerService - this class was created to register a custom document importer (AccessDBDocumentImporter)
+    public class CustomDocumentImportManagerService : DocumentImportManagerService {
+        IWorkbook _currentWorkbook;
+        public IWorkbook CurrentWorkbook { get { return _currentWorkbook; } }
+        public CustomDocumentImportManagerService(IWorkbook wb) : base() { _currentWorkbook = wb; }
+
+        protected override void RegisterNativeFormats() {
+            base.RegisterNativeFormats();
+            RegisterImporter(new AccessDBDocumentImporter(this));
+        }    
+    }
+
+    // custom Access Database importer - this class contains implementation of a custom LoadDocument method and manages the "Open File Dialog" filters
+    public class AccessDBDocumentImporter : IImporter<DocumentFormat, bool> {
+        #region IImporter<DocumentFormat,bool> Members
+        CustomDocumentImportManagerService currentService;
+
+        public AccessDBDocumentImporter(CustomDocumentImportManagerService service) { currentService = service; }
+
+        public FileDialogFilter Filter {
+            get { return new FileDialogFilter("MS Access Database files", "mdb" ); }
+        }
+
+        public DocumentFormat Format {
+            get { return DocumentFormat.Undefined; }
+        }
+
+        public bool LoadDocument(DevExpress.Office.IDocumentModel documentModel, System.IO.Stream stream, DevExpress.Office.Import.IImporterOptions options) {
+            return LoadDocument(documentModel, stream, options, true);
+        }
+
+        public bool LoadDocument(DevExpress.Office.IDocumentModel documentModel, System.IO.Stream stream, IImporterOptions options, bool leaveOpen) {
+            if(currentService != null && currentService.CurrentWorkbook != null) {
+                currentService.CurrentWorkbook.LoadMSAccessFile((stream as System.IO.FileStream).Name);
+            }
+            return true;
+        }
+
+        public DevExpress.Office.Import.IImporterOptions SetupLoading() {
+            return new AccessDBDocumentImporterOptions();
+        }
+
+        #endregion
+    }
+
+    // custom DocumentImporterOptions - this class was created to be used in the AccessDBDocumentImporter.SetupLoading method only
+    public class AccessDBDocumentImporterOptions : DocumentImporterOptions {
+        protected override DocumentFormat Format {
+            get { return DocumentFormat.Undefined; }
+        }
+    }
+
+    // static class which provides the LoadMSAccessFile extension method in which a loading MDB files is implemented
+    public static class MSAccessImportHelper {
+        // add a "MS Access File" option into a standard Open File dialog
+        public static void ExtendOpenFileCommand(this SpreadsheetControl spreadSheet) {
+            IDocumentImportManagerService service = (IDocumentImportManagerService)spreadSheet.GetService(typeof(IDocumentImportManagerService));
+            CustomDocumentImportManagerService customService = new CustomDocumentImportManagerService(spreadSheet.Document);
+            spreadSheet.ReplaceService<IDocumentImportManagerService>(customService);
+        }
+
+        // establish a connection to MS Access Data Base and getting all user tables from the Data Base
+        public static void LoadMSAccessFile(this IWorkbook workbook, string fileName) {
+            try {
+                string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName;
+
+                // Create the dataset and add the Categories table to it:
+                OleDbConnection myAccessConn = null;
+                try {
+                    myAccessConn = new OleDbConnection(connectionString);
+                }
+                catch(Exception ex) {
+                    MessageBox.Show("Error: Failed to create a database connection. \r\n" + ex.Message, "Error");
+                    return;
+                }
+                try {
+                    myAccessConn.Open();
+                    workbook.BeginUpdate();
+                    workbook.CreateNewDocument();
+                    // We only want user tables, not system tables
+                    string[] restrictions = new string[4];
+                    restrictions[3] = "Table";
+                    // Get list of user tables
+                    DataTable userTables = myAccessConn.GetSchema("Tables", restrictions);
+                    // Getting the data for every user tables
+                    foreach(DataRow userTablesRow in userTables.Rows) {
+                        string currentTableName = userTablesRow["TABLE_NAME"].ToString();
+                        using(OleDbCommand cmd = new OleDbCommand(string.Format("SELECT * FROM [{0}]", currentTableName), myAccessConn)) {
+                            using(OleDbDataAdapter adapter = new OleDbDataAdapter(cmd)) {
+                                DataTable currentDataTable = new DataTable(currentTableName);
+                                adapter.Fill(currentDataTable);
+                                LoadDataTableContentIntoSpreadsheetControl(workbook, currentDataTable, userTables.Rows.IndexOf(userTablesRow));
+                            }
+                        }
+                    }
+                    workbook.EndUpdate();
+                }
+                catch(Exception ex) {
+                    MessageBox.Show("Error: Failed to retrieve the required data from the DataBase.\r\n" + ex.Message, "Error");
+                    return;
+                }
+                finally {
+                    myAccessConn.Close();
+                }
+            }
+            catch(Exception ex) {
+                MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+            }
+        }
+
+        // load a corresponding Data Table into a separate Worksheet
+        static void LoadDataTableContentIntoSpreadsheetControl(IWorkbook wb, DataTable currentTable, int tableIndex) {
+            Worksheet workSheet = wb.Worksheets[0];
+            if(tableIndex > 0) {
+                workSheet = wb.Worksheets.Add();
+            }
+            wb.Worksheets[tableIndex].Name = currentTable.TableName;
+
+            for(int i = 0; i < currentTable.Columns.Count; i++) {
+                DataColumn currentColumn = currentTable.Columns[i];
+                workSheet.Cells[0, i].Value = currentColumn.ColumnName;
+            }
+
+            for(int i = 0; i < currentTable.Columns.Count; i++) {
+                DataColumn currentColumn = currentTable.Columns[i];
+                for(int j = 0; j < currentTable.Rows.Count; j++) {
+                    if(currentTable.Rows[j][i] == DBNull.Value) continue;
+                    object currentValue = currentTable.Rows[j][i];
+                    Type currentValueType = currentColumn.DataType;
+                    if(currentColumn.DataType == typeof(DateTime))
+                        workSheet.Cells[j + 1, i].Value = Convert.ToDateTime(currentTable.Rows[j][i]);
+                    else if(currentColumn.DataType == typeof(Int32))
+                        workSheet.Cells[j + 1, i].Value = Convert.ToInt32(currentTable.Rows[j][i]);
+                    else if(currentColumn.DataType == typeof(Int16))
+                        workSheet.Cells[j + 1, i].Value = Convert.ToInt16(currentTable.Rows[j][i]);
+                    else if(currentColumn.DataType == typeof(Single))
+                        workSheet.Cells[j + 1, i].Value = Convert.ToSingle(currentTable.Rows[j][i]);
+                    else if(currentColumn.DataType == typeof(decimal))
+                        workSheet.Cells[j + 1, i].Value = Convert.ToDouble(currentTable.Rows[j][i]);
+                    else
+                        workSheet.Cells[j + 1, i].Value = currentTable.Rows[j][i].ToString();
+                }
+            }
+            Table table = workSheet.Tables.Add(workSheet.GetDataRange(), true);
+            table.Style = wb.TableStyles[BuiltInTableStyleId.TableStyleLight1];
+            workSheet.Columns.AutoFit(0, workSheet.Columns.LastUsedIndex);
+        }
+    }
+}
---
+++ VB/WindowsFormsApplication1/Form1.Designer.vb
@@ -0,0 +1,203 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace WindowsFormsApplication1
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.spreadsheetControl1 = New DevExpress.XtraSpreadsheet.SpreadsheetControl()
+			Me.ribbonControl1 = New DevExpress.XtraBars.Ribbon.RibbonControl()
+			Me.spreadsheetCommandBarButtonItem1 = New DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem()
+			Me.spreadsheetCommandBarButtonItem2 = New DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem()
+			Me.spreadsheetCommandBarButtonItem3 = New DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem()
+			Me.spreadsheetCommandBarButtonItem4 = New DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem()
+			Me.spreadsheetCommandBarButtonItem5 = New DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem()
+			Me.spreadsheetCommandBarButtonItem6 = New DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem()
+			Me.spreadsheetCommandBarButtonItem7 = New DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem()
+			Me.spreadsheetCommandBarButtonItem8 = New DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem()
+			Me.spreadsheetCommandBarButtonItem9 = New DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem()
+			Me.spreadsheetCommandBarButtonItem10 = New DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem()
+			Me.fileRibbonPage1 = New DevExpress.XtraSpreadsheet.UI.FileRibbonPage()
+			Me.commonRibbonPageGroup1 = New DevExpress.XtraSpreadsheet.UI.CommonRibbonPageGroup()
+			Me.infoRibbonPageGroup1 = New DevExpress.XtraSpreadsheet.UI.InfoRibbonPageGroup()
+			Me.spreadsheetBarController1 = New DevExpress.XtraSpreadsheet.UI.SpreadsheetBarController()
+			CType(Me.ribbonControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.spreadsheetBarController1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' spreadsheetControl1
+			' 
+			Me.spreadsheetControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.spreadsheetControl1.Location = New System.Drawing.Point(0, 143)
+			Me.spreadsheetControl1.MenuManager = Me.ribbonControl1
+			Me.spreadsheetControl1.Name = "spreadsheetControl1"
+			Me.spreadsheetControl1.Size = New System.Drawing.Size(990, 579)
+			Me.spreadsheetControl1.TabIndex = 0
+			Me.spreadsheetControl1.Text = "spreadsheetControl1"
+			' 
+			' ribbonControl1
+			' 
+			Me.ribbonControl1.ExpandCollapseItem.Id = 0
+			Me.ribbonControl1.Items.AddRange(New DevExpress.XtraBars.BarItem() { Me.ribbonControl1.ExpandCollapseItem, Me.spreadsheetCommandBarButtonItem1, Me.spreadsheetCommandBarButtonItem2, Me.spreadsheetCommandBarButtonItem3, Me.spreadsheetCommandBarButtonItem4, Me.spreadsheetCommandBarButtonItem5, Me.spreadsheetCommandBarButtonItem6, Me.spreadsheetCommandBarButtonItem7, Me.spreadsheetCommandBarButtonItem8, Me.spreadsheetCommandBarButtonItem9, Me.spreadsheetCommandBarButtonItem10})
+			Me.ribbonControl1.Location = New System.Drawing.Point(0, 0)
+			Me.ribbonControl1.MaxItemId = 12
+			Me.ribbonControl1.Name = "ribbonControl1"
+			Me.ribbonControl1.Pages.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPage() { Me.fileRibbonPage1})
+			Me.ribbonControl1.Size = New System.Drawing.Size(990, 143)
+			' 
+			' spreadsheetCommandBarButtonItem1
+			' 
+			Me.spreadsheetCommandBarButtonItem1.CommandName = "FileNew"
+			Me.spreadsheetCommandBarButtonItem1.Id = 1
+			Me.spreadsheetCommandBarButtonItem1.Name = "spreadsheetCommandBarButtonItem1"
+			' 
+			' spreadsheetCommandBarButtonItem2
+			' 
+			Me.spreadsheetCommandBarButtonItem2.CommandName = "FileOpen"
+			Me.spreadsheetCommandBarButtonItem2.Id = 2
+			Me.spreadsheetCommandBarButtonItem2.Name = "spreadsheetCommandBarButtonItem2"
+			' 
+			' spreadsheetCommandBarButtonItem3
+			' 
+			Me.spreadsheetCommandBarButtonItem3.CommandName = "FileSave"
+			Me.spreadsheetCommandBarButtonItem3.Id = 3
+			Me.spreadsheetCommandBarButtonItem3.Name = "spreadsheetCommandBarButtonItem3"
+			' 
+			' spreadsheetCommandBarButtonItem4
+			' 
+			Me.spreadsheetCommandBarButtonItem4.CommandName = "FileSaveAs"
+			Me.spreadsheetCommandBarButtonItem4.Id = 4
+			Me.spreadsheetCommandBarButtonItem4.Name = "spreadsheetCommandBarButtonItem4"
+			' 
+			' spreadsheetCommandBarButtonItem5
+			' 
+			Me.spreadsheetCommandBarButtonItem5.CommandName = "FileQuickPrint"
+			Me.spreadsheetCommandBarButtonItem5.Id = 5
+			Me.spreadsheetCommandBarButtonItem5.Name = "spreadsheetCommandBarButtonItem5"
+			' 
+			' spreadsheetCommandBarButtonItem6
+			' 
+			Me.spreadsheetCommandBarButtonItem6.CommandName = "FilePrint"
+			Me.spreadsheetCommandBarButtonItem6.Id = 6
+			Me.spreadsheetCommandBarButtonItem6.Name = "spreadsheetCommandBarButtonItem6"
+			' 
+			' spreadsheetCommandBarButtonItem7
+			' 
+			Me.spreadsheetCommandBarButtonItem7.CommandName = "FilePrintPreview"
+			Me.spreadsheetCommandBarButtonItem7.Id = 7
+			Me.spreadsheetCommandBarButtonItem7.Name = "spreadsheetCommandBarButtonItem7"
+			' 
+			' spreadsheetCommandBarButtonItem8
+			' 
+			Me.spreadsheetCommandBarButtonItem8.CommandName = "FileUndo"
+			Me.spreadsheetCommandBarButtonItem8.Id = 8
+			Me.spreadsheetCommandBarButtonItem8.Name = "spreadsheetCommandBarButtonItem8"
+			' 
+			' spreadsheetCommandBarButtonItem9
+			' 
+			Me.spreadsheetCommandBarButtonItem9.CommandName = "FileRedo"
+			Me.spreadsheetCommandBarButtonItem9.Id = 9
+			Me.spreadsheetCommandBarButtonItem9.Name = "spreadsheetCommandBarButtonItem9"
+			' 
+			' spreadsheetCommandBarButtonItem10
+			' 
+			Me.spreadsheetCommandBarButtonItem10.CommandName = "FileShowDocumentProperties"
+			Me.spreadsheetCommandBarButtonItem10.Id = 10
+			Me.spreadsheetCommandBarButtonItem10.Name = "spreadsheetCommandBarButtonItem10"
+			' 
+			' fileRibbonPage1
+			' 
+			Me.fileRibbonPage1.Groups.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageGroup() { Me.commonRibbonPageGroup1, Me.infoRibbonPageGroup1})
+			Me.fileRibbonPage1.Name = "fileRibbonPage1"
+			' 
+			' commonRibbonPageGroup1
+			' 
+			Me.commonRibbonPageGroup1.ItemLinks.Add(Me.spreadsheetCommandBarButtonItem1)
+			Me.commonRibbonPageGroup1.ItemLinks.Add(Me.spreadsheetCommandBarButtonItem2)
+			Me.commonRibbonPageGroup1.ItemLinks.Add(Me.spreadsheetCommandBarButtonItem3)
+			Me.commonRibbonPageGroup1.ItemLinks.Add(Me.spreadsheetCommandBarButtonItem4)
+			Me.commonRibbonPageGroup1.ItemLinks.Add(Me.spreadsheetCommandBarButtonItem5)
+			Me.commonRibbonPageGroup1.ItemLinks.Add(Me.spreadsheetCommandBarButtonItem6)
+			Me.commonRibbonPageGroup1.ItemLinks.Add(Me.spreadsheetCommandBarButtonItem7)
+			Me.commonRibbonPageGroup1.ItemLinks.Add(Me.spreadsheetCommandBarButtonItem8)
+			Me.commonRibbonPageGroup1.ItemLinks.Add(Me.spreadsheetCommandBarButtonItem9)
+			Me.commonRibbonPageGroup1.Name = "commonRibbonPageGroup1"
+			' 
+			' infoRibbonPageGroup1
+			' 
+			Me.infoRibbonPageGroup1.ItemLinks.Add(Me.spreadsheetCommandBarButtonItem10)
+			Me.infoRibbonPageGroup1.Name = "infoRibbonPageGroup1"
+			' 
+			' spreadsheetBarController1
+			' 
+			Me.spreadsheetBarController1.BarItems.Add(Me.spreadsheetCommandBarButtonItem1)
+			Me.spreadsheetBarController1.BarItems.Add(Me.spreadsheetCommandBarButtonItem2)
+			Me.spreadsheetBarController1.BarItems.Add(Me.spreadsheetCommandBarButtonItem3)
+			Me.spreadsheetBarController1.BarItems.Add(Me.spreadsheetCommandBarButtonItem4)
+			Me.spreadsheetBarController1.BarItems.Add(Me.spreadsheetCommandBarButtonItem5)
+			Me.spreadsheetBarController1.BarItems.Add(Me.spreadsheetCommandBarButtonItem6)
+			Me.spreadsheetBarController1.BarItems.Add(Me.spreadsheetCommandBarButtonItem7)
+			Me.spreadsheetBarController1.BarItems.Add(Me.spreadsheetCommandBarButtonItem8)
+			Me.spreadsheetBarController1.BarItems.Add(Me.spreadsheetCommandBarButtonItem9)
+			Me.spreadsheetBarController1.BarItems.Add(Me.spreadsheetCommandBarButtonItem10)
+			Me.spreadsheetBarController1.Control = Me.spreadsheetControl1
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(990, 722)
+			Me.Controls.Add(Me.spreadsheetControl1)
+			Me.Controls.Add(Me.ribbonControl1)
+			Me.Name = "Form1"
+			Me.Ribbon = Me.ribbonControl1
+			Me.Text = "Form1"
+'			Me.Load += New System.EventHandler(Me.Form1_Load);
+			CType(Me.ribbonControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.spreadsheetBarController1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+			Me.PerformLayout()
+
+		End Sub
+
+		#End Region
+
+		Private spreadsheetControl1 As DevExpress.XtraSpreadsheet.SpreadsheetControl
+		Private ribbonControl1 As DevExpress.XtraBars.Ribbon.RibbonControl
+		Private spreadsheetCommandBarButtonItem1 As DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem
+		Private spreadsheetCommandBarButtonItem2 As DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem
+		Private spreadsheetCommandBarButtonItem3 As DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem
+		Private spreadsheetCommandBarButtonItem4 As DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem
+		Private spreadsheetCommandBarButtonItem5 As DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem
+		Private spreadsheetCommandBarButtonItem6 As DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem
+		Private spreadsheetCommandBarButtonItem7 As DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem
+		Private spreadsheetCommandBarButtonItem8 As DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem
+		Private spreadsheetCommandBarButtonItem9 As DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem
+		Private spreadsheetCommandBarButtonItem10 As DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem
+		Private fileRibbonPage1 As DevExpress.XtraSpreadsheet.UI.FileRibbonPage
+		Private commonRibbonPageGroup1 As DevExpress.XtraSpreadsheet.UI.CommonRibbonPageGroup
+		Private infoRibbonPageGroup1 As DevExpress.XtraSpreadsheet.UI.InfoRibbonPageGroup
+		Private spreadsheetBarController1 As DevExpress.XtraSpreadsheet.UI.SpreadsheetBarController
+	End Class
+End Namespace
+
